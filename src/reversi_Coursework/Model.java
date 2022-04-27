8976e5ff9e5784ba8929c9ba9e8c9bb526ccf9c5
alexgogitter <alex@avoinvents.com>
2022-04-27 13:54:14 +0100

    Hopefully I have completed the piece claiming algorithm. After some testing I can work on game completion and then the AI move

--- src/reversi_Coursework/Model.java
+++ src/reversi_Coursework/Model.java
@@ -21,11 +21,21 @@ public class Model {
 	
 	int[] nonEdgeCaseTransforms = {-9, -8, -7, -1, 0, 1, 7, 8, 9};
 	
-	int[] leftEdgeCaseTransforms = {-8, -7,  1,  8, 9};
-	int[] rightEdgeCaseTransforms = {-9, -8, -1, 7, 8};
-	int[] topEdgeCaseTransforms = {-1, 1, 7, 8, 9};
-	int[] bottomEdgeCaseTransforms = {-9, -8, -7, -1, 1};
+	int[] leftEdgeCaseTransforms = {-8, -7,  0, 1,  8, 9};
+	int[] rightEdgeCaseTransforms = {-9, -8, -1, 0, 7, 8};
+	int[] topEdgeCaseTransforms = {-1, 0, 1, 7, 8, 9};
+	int[] bottomEdgeCaseTransforms = {-9, -8, -7, -1, 0, 1};
 	
+	public boolean isIn(int[] haystack, int needle)
+	{
+		for(int i=0; i<haystack.length; i++)
+		{
+			if(haystack[i]==needle)
+				return true;
+		}
+		
+		return false;
+	}
 	
 	private class PotentialMove{//A struct-like class to help me with associating potential moves and the pieces taken when that move is maid
 		private int moveIndex;
@@ -70,20 +80,14 @@ public class Model {
 	
 	public void makeMove(int index, Piece p)
 	{
-		
-		System.out.println("potentialMoves.size(): " + potentialMoves.size() );
 		for(int i=0; i<potentialMoves.size(); i++)
 		{
-			
-			System.out.println("index: " + index +", potentialMoves.get(i).getMoveIndex():" + potentialMoves.get(i).getMoveIndex());
-			
 			if(potentialMoves.get(i).getMoveIndex()==index)
 			{
 				ArrayList<Integer> piecesToMove = potentialMoves.get(i).getPieces();
-				System.out.println("PiecesToMove.Size() "+ piecesToMove.size());
 				for(int j = 0; j<piecesToMove.size(); j++)
 				{
-					pieces[piecesToMove.get(j)]=p;
+					pieces[piecesToMove.get(j)]=currentTurn;
 				}
 			}
 		}
@@ -105,37 +109,34 @@ public class Model {
 				
 	}
 	
-	public int[] generateMaxPositions(int index)
-	{
-		
-	}
-	
 	public void clearPotentialMoves()
 	{
 		for (int i = 0 ; i < pieces.length ; i++)
 		{
 			if(pieces[i]==Piece.POTENTIAL)
 				pieces[i]=Piece.EMPTY;
-			
-			potentialMoves.clear();
-			
 		}
+		potentialMoves.clear();
 	}
 	
 	public void updatePotentialMoves()
 	{
-		System.out.println("Updating Potential Moves");
 		
 		for(int i=0; i<pieces.length; i++)
 		{
-			if(pieces[i]==Piece.EMPTY)
+			
+			if(pieces[i]==Piece.EMPTY&&validatePoint(i))
 			{
-				if(validatePoint(i))
-				{
-					pieces[i]=Piece.POTENTIAL;
-				}
+				pieces[i]=Piece.POTENTIAL;
 			}
 		}
+	}
+	
+	private boolean generalCase(int i1)
+	{
+		return !isIn(edgeCasesLeft, i1)&&!isIn(edgeCasesRight, i1)
+		&&!isIn(edgeCasesTop, i1)&&!isIn(edgeCasesBottom, i1)
+		&&!isIn(cornerCases, i1);
 	}
 	
 	private PotentialMove processPoints(PotentialMove m, int[] transforms, int index)
@@ -144,35 +145,196 @@ public class Model {
 		{	
 			
 			int offset=transforms[k];
-			int i = index;
+			int i = index, lowerLimit=0, upperLimit=63;
 			
-			
-			if(pieces[i]==Piece.EMPTY&&pieces[i+offset]==getOppositePiece(currentTurn))
+			if(offset==1||offset==-1)
 			{
-				while((i>=0&&i<=63))
+				lowerLimit=(index/8)*8;
+				upperLimit=lowerLimit+7;
+			}
+			else if(offset==9||offset==-9)
+			{
+				int i1=index;
+				if((index!=0)&&(index!=63))
 				{
-				
-					i+=offset;
-					if(pieces[i]==currentTurn)
+					if(generalCase(i1))
 					{
-						int j = i;
-						boolean valid=true;
-						
-						
-						while(j!=index&&valid)
+						while(generalCase(i1))
 						{
-							j-=offset;
-							m.addTakenPiece(j);
+							i1+=offset;
+						}							
+					}
+					else if(isIn(edgeCasesLeft, i1) && !isIn(cornerCases, i1))
+					{
+						while(!isIn(edgeCasesRight, i1)&&!isIn(edgeCasesTop, i1)
+								&&!isIn(edgeCasesBottom, i1)&&!isIn(cornerCases, i1))
+						{
+							i1+=offset;
 						}
+					}
+					else if(isIn(edgeCasesRight, i1) && !isIn(cornerCases, i1))
+					{
+						while(!isIn(edgeCasesLeft, i1)&&!isIn(edgeCasesTop, i1)
+								&&!isIn(edgeCasesBottom, i1)&&!isIn(cornerCases, i1))
+						{
+							i1+=offset;
+						}
+					}
+					else if(isIn(edgeCasesTop, i1) && !isIn(cornerCases, i1))
+					{
+						while(!isIn(edgeCasesRight, i1)&&!isIn(edgeCasesLeft, i1)
+								&&!isIn(edgeCasesBottom, i1)&&!isIn(cornerCases, i1))
+						{
+							i1+=offset;
+						}
+					}
+					else if(isIn(edgeCasesBottom, i1) && !isIn(cornerCases, i1))
+					{
+						while(!isIn(edgeCasesRight, i1)&&!isIn(edgeCasesTop, i1)
+								&&!isIn(edgeCasesLeft, i1)&&!isIn(cornerCases, i1))
+						{
+							i1+=offset;
+						}
+					}
+					if(offset>0)
+					{
+						upperLimit=i1;
+					}
+					else if(offset<0)
+					{
+						lowerLimit=i1;
+					}
+					
+				}
+				else if(index==0&&offset==9)
+				{
+					upperLimit=63;
+				}
+				else if(index==63&&offset==-9)
+				{
+					lowerLimit=0;
+				}
+								
+			}
+			else if(offset==7||offset==-7)
+			{
+				int i1=index;
+				if((index!=7)&&(index!=56))
+				{	
+					if(generalCase(i1))
+					{
+						while(generalCase(i1))
+						{
+							i1+=offset;
+						}							
+					}
+					else if(isIn(edgeCasesLeft, i1) && !isIn(cornerCases, i1))
+					{
+						while(!isIn(edgeCasesRight, i1)&&!isIn(edgeCasesTop, i1)
+								&&!isIn(edgeCasesBottom, i1)&&!isIn(cornerCases, i1))
+						{
+							i1+=offset;
+						}
+					}
+					else if(isIn(edgeCasesRight, i1) && !isIn(cornerCases, i1))
+					{
+						while(!isIn(edgeCasesLeft, i1)&&!isIn(edgeCasesTop, i1)
+								&&!isIn(edgeCasesBottom, i1)&&!isIn(cornerCases, i1))
+						{
+							i1+=offset;
+						}
+					}
+					else if(isIn(edgeCasesTop, i1) && !isIn(cornerCases, i1))
+					{
+						while(!isIn(edgeCasesRight, i1)&&!isIn(edgeCasesLeft, i1)
+								&&!isIn(edgeCasesBottom, i1)&&!isIn(cornerCases, i1))
+						{
+							i1+=offset;
+						}
+					}
+					else if(isIn(edgeCasesBottom, i1) && !isIn(cornerCases, i1))
+					{
+						while(!isIn(edgeCasesRight, i1)&&!isIn(edgeCasesTop, i1)
+								&&!isIn(edgeCasesLeft, i1)&&!isIn(cornerCases, i1))
+						{
+							i1+=offset;
+						}
+					}
+					if(offset>0)
+					{
+						upperLimit=i1;
+					}
+					else if(offset<0)
+					{
+						lowerLimit=i1;
+					}
+				}
+				else if(index==7&&offset==7)
+				{
+					upperLimit=56;
+				}
+				else if(index==56&&offset==-7)
+				{
+					lowerLimit=7;
+				}
+			}
+			
+			//System.out.println("index: "+ index +". offset:" + offset + ". Limits: " + lowerLimit + ", " + upperLimit);
+			
+			if(pieces[index+offset]==getOppositePiece(currentTurn))
+			{
+				boolean validDirection=true;
+				i+=offset;
+				while((i>=lowerLimit&&i<=upperLimit)&&pieces[i]!=currentTurn)
+				{
+					if(pieces[i]!=getOppositePiece(currentTurn))
+					{
+						System.out.println("A B");
+						validDirection=false;
 						break;
 					}
-					else if(pieces[i]==Piece.EMPTY)
-						break;
-					
-				}			
+					i+=offset;
+				}
+				
+				if(validDirection)
+				{
+					System.out.println("B");
+					int j=index;
+					while(j!=i)
+					{
+						m.addTakenPiece(j);
+						j+=offset;
+					}
+				}
 			}
+//			if(pieces[index+offset]==getOppositePiece(currentTurn))//If we are next to one of the opponents pieces
+//			{
+//				
+////				while(pieces[i]!=currentTurn)
+////				{
+////					i+=offset;
+////					if(!(i<=upperLimit&&i>=lowerLimit))
+////					{
+////						i-=offset;
+////						break;
+////					}
+////				}
+////				
+////				if(pieces[i]==currentTurn)
+////				{
+////					int j = index+offset;
+////					
+////					while(j!=i&&pieces[j]!=Piece.EMPTY)
+////					{
+////						m.addTakenPiece(j);
+////						j+=offset;	
+////					}
+////				}	
+//				
+//				
+//				
+//			}
 		}
-		
 		return m;
 		
 	}
@@ -182,49 +344,52 @@ public class Model {
 		
 		PotentialMove m = new PotentialMove();
 		m.setMoveIndex(index);
-		
-		if(Arrays.binarySearch(edgeCasesLeft, index) < 0 && Arrays.binarySearch(edgeCasesRight, index) < 0  &&
-				Arrays.binarySearch(edgeCasesTop, index) < 0  && Arrays.binarySearch(edgeCasesBottom, index) < 0 &&
-				Arrays.binarySearch(cornerCases, index) < 0)//General Case
+		if(generalCase(index))//General Case
 		{
 			processPoints(m, nonEdgeCaseTransforms, index);
+			
 		}
-		else if (Arrays.binarySearch(edgeCasesLeft, index) > 0 && Arrays.binarySearch(cornerCases, index) < 0)
+		else if (isIn(edgeCasesLeft, index) && !isIn(cornerCases, index))
 		{
 			processPoints(m, leftEdgeCaseTransforms, index);
+			
 		}
-		else if (Arrays.binarySearch(edgeCasesRight, index) > 0 && Arrays.binarySearch(cornerCases, index) < 0)
+		else if (isIn(edgeCasesRight, index) && !isIn(cornerCases, index))
 		{
 			processPoints(m, rightEdgeCaseTransforms, index);
+			
 		}
-		else if (Arrays.binarySearch(edgeCasesTop, index) > 0 && Arrays.binarySearch(cornerCases, index) < 0)
+		else if (isIn(edgeCasesTop, index) && !isIn(cornerCases, index))
 		{
 			processPoints(m, topEdgeCaseTransforms, index);
+			
 		}
-		else if (Arrays.binarySearch(edgeCasesBottom, index) > 0 && Arrays.binarySearch(cornerCases, index) < 0)
+		else if (isIn(edgeCasesBottom, index) && !isIn(cornerCases, index))
 		{
 			processPoints(m, bottomEdgeCaseTransforms, index);
+			
 		}
-		else if (Arrays.binarySearch(cornerCases, index) >0)
+		else if (Arrays.binarySearch(cornerCases, index) >=0)
 		{
+			
 			if(index==0)
 			{
-				int[] i= {1,8,9};
+				int[] i= {0, 1, 8, 9};
 				processPoints(m, i, index);
 			}
 			else if(index==7)
 			{
-				int[] i= {-1,7,8};
+				int[] i= {-1, 0, 7, 8};
 				processPoints(m, i, index);
 			}
 			else if(index==56)
 			{
-				int[] i= {1,-8,-7};
+				int[] i= {1, 0, -8, -7};
 				processPoints(m, i, index);
 			}
 			else if(index==63)
 			{
-				int[] i= {-1,-8,-9};
+				int[] i= {-1, 0, -8,-9};
 				processPoints(m, i, index);
 			}
 		}
